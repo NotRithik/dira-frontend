91e19dcfc7fe56eb72e880b6692f3538d3dbd0ac
Rithik Kumar S <rithikum123@gmail.com>
2025-01-27 09:01:52 +0530

    Remove the horrendous landing page text and update it with something nicer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="text-center mb-16 mt-48">
         <h1 className="text-6xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600 p-2">
-          Introducing Dira
+          Welcome to Dira
         </h1>
         <p className="text-xl mb-8 max-w-2xl">
-          The OM-backed stablecoin for the Emirati Dirham. Secure, stable, and decentralized.
+          The premier decentralized stablecoin for the Emirati Dirham, backed by OM and built for the future of finance in Dubai.
         </p>
       </div>
 
@@ -24,25 +24,25 @@ export default function Home() {
         </div>
         <p className="mb-32"></p>
 
-        <div className="text-left mb-16">
+        <div className="text-left mb-28">
           <h2 className="w-2/3 text-3xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600">
-            Stable Meets Agile
+            Stability in a Volatile World
           </h2>
           <p className="w-2/3 text-xl">
-            Crypto volatility? Not here. Dira transforms your OM into a rock-solid digital dirham, turning crypto complexity into simple, reliable value.
+            Navigate crypto markets with confidence. Dira offers a stable, AED-pegged digital currency, allowing you to transact with the reliability of traditional finance and the innovation of DeFi. Secure your assets against market fluctuations and embrace predictable value.
           </p>
         </div>
 
         <div className="text-right mb-16">
           <h2 className="ml-auto w-2/3 text-3xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600">
-            Dubai&apos;s Financial Frontier
+            Unlocking Dubai's Digital Economy
           </h2>
           <p className="ml-auto w-2/3 text-xl" >
-            The RWA market is booming. Dubai needs liquidity. We&apos;re building the bridge. How would you like to join us?
+            Dubai is rapidly becoming a global hub for Real World Asset tokenization and digital innovation. Dira is designed to be a foundational layer for this growth, providing essential on-chain liquidity for AED and bridging the gap between traditional markets and the decentralized future. Join us in building the financial frontier of Dubai.
           </p>
         </div >
 
-        <div className="flex space-x-4 mb-6 w-full justify-center">
+        <div className="flex space-x-4 mt-32 w-full justify-center">
           <Link href="/manage-collateral">
             <Button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-32">
               Lock OM
